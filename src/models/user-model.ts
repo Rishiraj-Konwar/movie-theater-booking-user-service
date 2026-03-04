cb47a9a82795ce5db83cd7ed685faec1248668c5
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-03-04 21:09:50 +0530

    set up the models

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -8,4 +8,29 @@ export interface IUser extends Document{
   location: string
 }
 
-const userSchema: Schema<IUser> = new Schema  
+const userSchema: Schema<IUser> = new Schema({
+  userName:{
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    required: true
+  },
+  location:{
+    type: String,
+    rquired: true
+  }
+})
+
+export const User = mongoose.model<IUser>("User", userSchema)
