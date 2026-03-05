553f35d5ec0b09a43bb1b9a910c1490655248a27
Rishiraj-Konwar <rrkrskkonwar@gmail.com>
2026-03-05 22:15:28 +0530

    add the get by email fucntion

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,8 +1,14 @@
+import type { Model } from "mongoose";
 import type { IUser } from "../models/user-model";
+import { User } from "../models/user-model";
 import { CrudRepository } from "./crud-repository";
 
 export class UserRepository extends CrudRepository<IUser>{
   constructor(){
-    super
+    super(User as Model<IUser>)
+  }
+  async getByEmail(email: string){
+    const response = await this.model.findOne({email})
+    return response
   }
 }
